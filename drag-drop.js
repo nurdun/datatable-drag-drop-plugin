5100ba0dd0df916258be5b5b6436781c0e112650
nurdun <nurdun1993@hotmail.com>
2018-04-12 14:53:18 +0800

    : add if statment

--- drag-drop.js
+++ drag-drop.js
@@ -51,45 +51,47 @@ class TableDragAndDrop{
             let index=ev.dataTransfer.getData("index");
             let idx = _that.datatable.row( this ).index();
             dataType = this.getAttribute('category');
-            let currentIdx = idx;
-            let nextDataType;
-            do {
-                currentIdx++;
-                nextDataType = _that.datatable.rows(currentIdx).nodes()[0].getAttribute('category');
-                nextArr.push(currentIdx-1);
-            } while (nextDataType != dataType);
-            for(let i in _that.arr){
-                newArr.push(testData[_that.arr[i]]);
-            }
-            for(let i in nextArr){
-                nextArrObj.push(testData[nextArr[i]]);
-            }
-            testData.splice(_that.arr[0], _that.arr.length);
-            for(let i in nextArrObj){
-                if(i<nextArrObj.length-1){
-                    testData.insert((Number(index)+Number(i)),nextArrObj[i]);
+            if(dataType == _that.dataType){
+                let currentIdx = idx;
+                let nextDataType;
+                do {
+                    currentIdx++;
+                    nextDataType = _that.datatable.rows(currentIdx).nodes()[0].getAttribute('category');
+                    nextArr.push(currentIdx-1);
+                } while (nextDataType != dataType);
+                for(let i in _that.arr){
+                    newArr.push(testData[_that.arr[i]]);
                 }
-            }
-            if(idx>=index){
-                testData.splice(idx-(_that.arr.length-nextArr.length), nextArr.length);
-                for(let i in newArr){
-                    if(i<newArr.length-1){
-                        testData.insert((idx+Number(i)+nextArr.length)-_that.arr.length,newArr[i]);
+                for(let i in nextArr){
+                    nextArrObj.push(testData[nextArr[i]]);
+                }
+                testData.splice(_that.arr[0], _that.arr.length);
+                for(let i in nextArrObj){
+                    if(i<nextArrObj.length-1){
+                        testData.insert((Number(index)+Number(i)),nextArrObj[i]);
                     }
                 }
-            }else {
-                testData.splice(idx, nextArr.length);
-                for(let i in newArr){
-                    if(i<newArr.length-1){
-                        testData.insert(idx+Number(i),newArr[i]);
+                if(idx>=index){
+                    testData.splice(idx-(_that.arr.length-nextArr.length), nextArr.length);
+                    for(let i in newArr){
+                        if(i<newArr.length-1){
+                            testData.insert((idx+Number(i)+nextArr.length)-_that.arr.length,newArr[i]);
+                        }
+                    }
+                }else {
+                    testData.splice(idx, nextArr.length);
+                    for(let i in newArr){
+                        if(i<newArr.length-1){
+                            testData.insert(idx+Number(i),newArr[i]);
+                        }
                     }
                 }
+                newTestData = testData;
+                this.data = newTestData;
+                _that.datatable.clear();
+                _that.datatable.rows.add(newTestData); // Add new data
+                _that.datatable.columns.adjust().draw();
             }
-            newTestData = testData;
-            this.data = newTestData;
-            _that.datatable.clear();
-            _that.datatable.rows.add(newTestData); // Add new data
-            _that.datatable.columns.adjust().draw();
         });
     }
 }
